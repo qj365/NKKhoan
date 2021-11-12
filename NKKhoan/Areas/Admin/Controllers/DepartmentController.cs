56571c05fdba32bf868922522d501c8f6faf19f7
QJ <quangjunior365@gmail.com>
2021-11-12 23:23:18 +0700

    sửa layout

---
+++ NKKhoan/Areas/Admin/Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NKKhoan.Areas.Admin.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Admin/Department
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
