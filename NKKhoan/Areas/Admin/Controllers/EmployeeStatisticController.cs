f5a44d3290f2510f8da00de49d60ba74cd7529a4
QJ <quangjunior365@gmail.com>
2021-12-08 15:50:34 +0700

    xong dashboard

---
+++ NKKhoan/Areas/Admin/Controllers/EmployeeStatisticController.cs
@@ -0,0 +1,72 @@
+﻿using NKKhoan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Data.Entity;
+using NKKhoan.Areas.Admin.ViewModel;
+using System.Globalization;
+
+namespace NKKhoan.Areas.Admin.Controllers
+{
+    public class EmployeeStatisticController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public EmployeeStatisticController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Admin/EmployeeStatistic
+        public ActionResult Index(string ngaybatdau = null, string ngayketthuc = null)
+        {
+            var employees = _context.CongNhan.Include(c => c.PhongBan).Include(c => c.ChucVu).ToList();
+            var list = new List<EmployeeStatisticViewModel>();
+
+            var nbd = DateTime.ParseExact("01/01/1753", "dd/MM/yyyy", CultureInfo.InvariantCulture); 
+            var nkt = DateTime.ParseExact("31/12/9999", "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            if (!String.IsNullOrWhiteSpace(ngaybatdau))
+            {
+                nbd = DateTime.ParseExact(ngaybatdau, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            }
+
+            if (!String.IsNullOrWhiteSpace(ngayketthuc))
+            {
+                nkt = DateTime.ParseExact(ngayketthuc, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            }
+
+            foreach (var employee in employees)
+            {
+                var item = new EmployeeStatisticViewModel(employee);
+
+                int luong = 0;
+                double ngaycong = 0;
+
+                try
+                {
+                    luong = _context.Database.SqlQuery<int>("select dbo.TienLuongSanPhamCongNhan({0}, {1}, {2})", new object[] { item.MaNhanCong, nbd, nkt }).First();
+                    ngaycong = _context.Database.SqlQuery<double>("select dbo.NhatKyLamViec({0}, {1}, {2})", new object[] { item.MaNhanCong, nbd, nkt }).First();
+                }
+                catch { }                 
+                            
+
+                item.LuongSP = luong;
+                item.NgayCong = ngaycong;
+
+                list.Add(item);
+            }
+
+            return View(list);
+        }
+    }
+}
---
+++ NKKhoan/Areas/Admin/ViewModel/EmployeeStatisticViewModel.cs
@@ -0,0 +1,50 @@
+﻿using NKKhoan.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Xunit;
+
+namespace NKKhoan.Areas.Admin.ViewModel
+{
+    public class EmployeeStatisticViewModel
+    {
+        public EmployeeStatisticViewModel()
+        {
+
+        }
+
+        public EmployeeStatisticViewModel(CongNhan congNhan)
+        {
+            HoTen = congNhan.HoTen;
+            MaNhanCong = congNhan.MaNhanCong;
+            PhongBan = congNhan.PhongBan;
+            ChucVu = congNhan.ChucVu;
+            LuongSP = 0;
+            NgayCong = 0;
+
+        }
+
+        [Key]
+        [Display(Name = "Mã công nhân")]
+        public int MaNhanCong { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Họ tên")]
+        [Required(ErrorMessage = "Vui lòng nhập họ tên")]
+        public string HoTen { get; set; }
+
+        [Display(Name = "Lương sản phẩm")]
+        public int? LuongSP { get; set; }
+
+        [Display(Name = "Ngày công")]
+        public double? NgayCong { get; set; }
+
+        public virtual PhongBan PhongBan { get; set; }
+
+        public virtual ChucVu ChucVu { get; set; }
+
+
+    }
+}
