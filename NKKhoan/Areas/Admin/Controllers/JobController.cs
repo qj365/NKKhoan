c68696c2b8da05d7c4caacdcf7e877f2517f507d
Anger000 <77683727+khue24112000@users.noreply.github.com>
2021-12-10 01:25:23 +0700

    Xong CongViec

--- NKKhoan/Areas/Admin/Controllers/JobController.cs
+++ NKKhoan/Areas/Admin/Controllers/JobController.cs
@@ -1,10 +1,12 @@
-﻿using NKKhoan.Areas.Admin.ViewModel;
-using NKKhoan.Models;
+﻿using NKKhoan.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Data.Entity;
+using System.Globalization;
 
 
 namespace NKKhoan.Areas.Admin.Controllers
@@ -21,33 +23,44 @@ namespace NKKhoan.Areas.Admin.Controllers
             _context.Dispose();
         }
         // GET: Admin/Job
-        public ActionResult Index()
+        public ActionResult Index(int? minprice = null, int? maxprice = null, int? ssp = null, string stv = null, string sb = null)
         {
-            var job = _context.CongViec.ToList();
+            ViewBag.SanPham = _context.SanPham.ToList();
+            var jobQuery = _context.CongViec.Include(c => c.SanPham);
+            if (!String.IsNullOrWhiteSpace(sb))
+                jobQuery = jobQuery.Where(c => c.TenCongViec.Contains(sb));
+            if (minprice != null)
+                jobQuery = jobQuery.Where(c => c.DonGia >= minprice);
+
+            if (maxprice != null)
+                jobQuery = jobQuery.Where(c => c.DonGia  <= maxprice);
+            if (ssp != null)
+                jobQuery = jobQuery.Where(c => c.SanPham.MaSanPham == ssp);
+            if (!String.IsNullOrWhiteSpace(stv))
+            {
+                int tb = _context.Database.SqlQuery<int>("Select AVG(dongia) from CongViec ").FirstOrDefault();
+                if (stv == "tv1")
+                    jobQuery = jobQuery.Where(c => c.DonGia > tb);
+                if(stv == "tv2")
+                    jobQuery = jobQuery.Where(c => c.DonGia < tb);
+            }
+            var job = jobQuery.ToList();
             return View(job);
         }
         public ViewResult Create()
         {
-            var sp = _context.SanPham.ToList();
-            var viewModel = new JobViewModel
-            {
-                SanPhams = sp
-            };
+            ViewBag.SanPham = _context.SanPham.ToList();
 
-            return View("JobForm", viewModel);
+            return View();
         }
         public ActionResult Edit(int id)
         {
             var job = _context.CongViec.SingleOrDefault(c => c.MaCongViec == id);
             if (job == null)
                 return HttpNotFound();
-            var sp = _context.SanPham.ToList();
-            var viewModel = new JobViewModel
-            {
-                SanPhams = sp
-            };
+            ViewBag.SanPham = _context.SanPham.ToList();
 
-            return View("JobForm", viewModel);
+            return View(job);
         }
 
         public ActionResult Delete(int id)
